914b17a95fbf2fb65c6be22099a69b6c274d115b
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-06-18 20:31:24 +0200

    refactor: iterate map using charlist without converting input to string

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -10,27 +10,12 @@ defmodule RnaTranscription do
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
     dna_nucleotide_map = %{
-      :g => "c",
-      :c => "g",
-      :t => "a",
-      :a => "u"
+      ?G => ?C,
+      ?C => ?G,
+      ?T => ?A,
+      ?A => ?U
     }
 
-    # formats charlist input to iterable list
-    dna =
-      List.to_string(dna)
-      |> String.split("")
-      |> Enum.filter(fn value -> value !== "" end)
-      |> Enum.map(fn value -> String.downcase(value) end)
-
-    # throw away values that are not in the map
-    dna =
-      Enum.filter(dna, fn value -> Map.get(dna_nucleotide_map, String.to_atom(value)) !== nil end)
-
-    # map to rna
-    rna = Enum.map(dna, fn value -> Map.get(dna_nucleotide_map, String.to_atom(value)) end)
-
-    # format to desired output
-    Enum.join(rna, "") |> String.upcase() |> String.to_charlist()
+    Enum.map(dna, fn char -> dna_nucleotide_map[char] end)
   end
 end
